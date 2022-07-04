a6d06db77dc2d06ee27c45c700cfd08fb592e096
joseph maxime <mangajm004@gmail.com>
2022-07-04 18:28:43 +0000

    fix __name__

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-import add_o
+import add_0
 
 if __name__ == "__main__":
     a = 1
     b = 2
-    summ = add_o.add(a, b)
+    summ = add_0.add(a, b)
     print("{} + {} = {}".format(a, b, summ))
