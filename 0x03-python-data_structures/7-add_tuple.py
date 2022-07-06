7ebf0bc044defd2568d908e95ba51c0b51c57981
joseph maxime <mangajm004@gmail.com>
2022-07-06 22:44:59 +0000

    add file

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -6,23 +6,22 @@ def add_tuple(tuple_a=(), tuple_b=()):
     list_b = list(tuple_b)
 
     if len(list_a) == 1:
-        list_a = [list_a[0],0]
+        list_a = [list_a[0], 0]
     elif len(list_a) == 0:
-        list_a = [0,0]
+        list_a = [0, 0]
     else:
         list_a[2:] = []
 
     if len(list_b) == 1:
-        list_b = [list_b[0],0]
+        list_b = [list_b[0], 0]
     elif len(list_b) == 0:
-        list_b = [0,0]
+        list_b = [0, 0]
     else:
         list_b[2:] = []
 
-    l = [0,0]
-    l[0] = list_a[0] + list_b[0]
-    l[1] = list_a[1] + list_b[1]
+    list_a[0] = list_a[0] + list_b[0]
+    list_a[1] = list_a[1] + list_b[1]
 
-    tupl = tuple(l)
+    tupl = tuple(list_a)
 
     return tupl
