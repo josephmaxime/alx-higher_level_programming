16aa00cc3a84612f531ee4ea62c60aa9d24a0fe3
joseph maxime <mangajm004@gmail.com>
2022-07-12 14:51:44 +0000

    add file

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <Python.h>
+
+void print_python_list_info(PyObject *p)
+{
+	PyListObject list_ob;
+	long length;
+	PyObject *object;
+
+	list_ob = (PyListObject *)p;
+
+	length = PyList_Size(p);
+
+	printf("[*] Size of the Python List = %ld\n", length);
+	printf("[*] Allocated = %ld\n", list->allocated);
+
+	for (i = 0; i < length; i++)
+	{
+		object = PyList_GetItem(list_ob, i);
+		type = object->ob_type;
+		printf("Element %ld: %s\n", i, type->tp_name);
+	}
+	
+}
