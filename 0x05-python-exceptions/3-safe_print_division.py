62f772781969a526fbd3edf44a30d117e2b83d37
joseph maxime <mangajm004@gmail.com>
2022-07-19 22:31:31 +0000

    add file

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+
+    try:
+        result = (a / b)
+
+    except Exception:
+        result = None
+        return None
+
+    finally:
+        print("Inside result: {}".format(result))
+
+    return result
