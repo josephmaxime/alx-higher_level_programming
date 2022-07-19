86c47f295899a7947b62b232258a36496efb2bba
joseph maxime <mangajm004@gmail.com>
2022-07-19 22:07:49 +0000

    add file

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+
+    i = 0
+    for elt in range(x):
+        try:
+            print("{:d}".format(my_list[elt]), end="")
+            i += 1
+        except ValueError:
+            continue
+
+        except TypeError:
+           continue
+
+        except IndexError:
+            break
+
+    print()
+    return (i)
