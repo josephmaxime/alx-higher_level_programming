d811b4004ad1b18a4f6c240869532ca7c4fa20eb
joseph maxime <mangajm004@gmail.com>
2022-07-20 21:32:02 +0000

    add file

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+"""0-square Module"""
+
+
+class Square:
+
+    def __init__(self, size):
+
+        """Initialize variable of the square class
+
+        Args:
+
+           size (int): The size of a side of the square.
+
+        """
+
+        self.__size = size
