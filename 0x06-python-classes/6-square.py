d761699d740646e93fba84429de544d2c123c098
joseph maxime <mangajm004@gmail.com>
2022-07-22 08:58:05 +0000

    add file

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 
-"""5-square Module"""
+"""This Module define a square with #"""
 
 
 class Square:
-    """ Class square define a square """
+    """This Class square define a square """
 
     def __init__(self, size=0, position=(0, 0)):
 
