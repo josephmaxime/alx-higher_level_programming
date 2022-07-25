c6113b30307cead6ce32574ca201b834b49a66de
joseph maxime <mangajm004@gmail.com>
2022-07-25 10:01:53 +0000

    add file

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -5,7 +5,7 @@ import math
 class MagicClass:
     """This class define a cyrcle"""
 
-    def __init__(self, radius):
+    def __init__(self, radius=0):
         """Initialize variable"""
         self.__radius = 0
         if (type(radius) is not int and type(radius) is not float):
