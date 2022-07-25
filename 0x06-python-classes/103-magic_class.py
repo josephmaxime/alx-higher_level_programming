abfd10c991224c676da7c8c5e2b9d3f6e78d643a
joseph maxime <mangajm004@gmail.com>
2022-07-25 09:55:19 +0000

    add file

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Magic calculator radius module"""
+
+import math
+class MagicClass:
+    """This class define a cyrcle"""
+
+    def __init__(self, radius):
+        """Initialize variable"""
+        self.__radius = 0
+        if (type(radius) is not int and type(radius) is not float):
+            raise TypeError("radius must be a number")
+        self.__radius = radius
+
+    def area(self):
+        """Area of a cycle."""
+        return (self.__radius ** 2) * (math.pi)
+
+    def circumference(self):
+        """Circumference of cycle."""
+        return 2 * (math.pi) * (self.__radius)
