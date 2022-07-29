d6c43862ebbaac91086e2eb2ee196ac0cf0a4627
joseph maxime <mangajm004@gmail.com>
2022-07-29 15:24:23 +0000

    add file

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Module for lockedclass"""
+
 
 class LockedClass:
+    """ that prevents the user from dynamically
+    creating new instance attributes"""
 
      __slots__ = ('first_name')
