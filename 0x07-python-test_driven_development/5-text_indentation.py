bfe11e333be1e7c4c0bcc088100b909e9ced8132
joseph maxime <mangajm004@gmail.com>
2022-07-30 18:27:21 +0000

    add file

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+This is the "5-test_indentation" module.
+"""
+
+
+def text_indentation(text):
+    """splits a text into lines along "?", ":", "." followed by 2 new lines"""
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    space = 0
+
+    for letter in text:
+
+        if space == 0:
+
+            if letter == ' ':
+                continue
+
+            else:
+                space = 1
+
+        if space == 1:
+
+            if letter == '?' or letter == '.' or letter == ':':
+                print(letter)
+                print()
+
+                space = 0
+
+            else:
+                print(a, end="")
