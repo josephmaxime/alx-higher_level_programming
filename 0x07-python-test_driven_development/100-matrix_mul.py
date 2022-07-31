379535c7cb852a2e66ef748a83a2f85a6d631cf9
joseph maxime <mangajm004@gmail.com>
2022-07-31 23:09:47 +0000

    add file

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -59,7 +59,6 @@ def mult(m_a, m_b):
     "This is the function that multiply two matrix"
 
     res = [[0 for x in range(len(m_b[0]))] for y in range(len(m_a[0]))]
-    print(res)
 
     for i in range(len(m_a)):
 
