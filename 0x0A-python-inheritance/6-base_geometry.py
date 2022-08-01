a1b71c51caa02bf1adaf39746001089fd10b7418
joseph maxime <mangajm004@gmail.com>
2022-08-01 14:23:29 +0000

    add fil

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" base geometry module"""
+
+
+class BaseGeometry:
+    """Base geometry class"""
+
+    def __init__(self):
+        """Initialize"""
+
+        pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
