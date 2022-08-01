be2961676446555ce0a5796b9eccfb9173c28cbe
joseph maxime <mangajm004@gmail.com>
2022-08-01 01:44:06 +0000

    add file

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+Defines lazy_matrix module
+"""
+import numpy
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """ matrix multiplication of two matrices"""
+
+    return numpy.matmul(m_a, m_b)
