af17dcecce2807b86cebe8f52150a96ec71459b2
joseph maxime <mangajm004@gmail.com>
2022-08-02 17:33:06 +0000

    add 9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -9,9 +9,9 @@ class Student:
     def __init__(self, first_name, last_name, age):
         """ Initialize """
 
-        self.__first_name = first_name
-        self.__last_name = last_name
-        self.__age = age
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self):
         """Retrieves a dictionary representation of a Student instance."""
