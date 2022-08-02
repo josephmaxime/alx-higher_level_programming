aac355ff3f47bf8b9ad7a92230de2335027eaf41
joseph maxime <mangajm004@gmail.com>
2022-08-02 17:21:24 +0000

    add 8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """ Class to JSON module
 """
-to_j_s = import('3-to_json_string.py').to_json_string
+#to_j_s = import('3-to_json_string.py').to_json_string
 
 
 def class_to_json(obj):
     """function that returns the dictionary description
     with simple data structure for json serialisation"""
 
-    obj_ = to_j_s(obj.__dict__)
+    obj_ = (obj.__dict__)
 
     return obj_
