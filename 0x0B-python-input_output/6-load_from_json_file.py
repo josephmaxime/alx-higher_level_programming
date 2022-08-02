635c9c5ee461fd0df80e44c0bd59bd1eab67bb77
joseph maxime <mangajm004@gmail.com>
2022-08-02 15:26:48 +0000

    add 6-load_from_json_file.py

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" JSON module """
+import json
+
+
+def load_from_json_file(filename):
+    """function that create an objet (string)
+    from json representation file
+    """
+
+    with open(filename, 'w', encoding="utf-8") as f:
+
+        obj_ = json.load(f)
+
+    return obj_
