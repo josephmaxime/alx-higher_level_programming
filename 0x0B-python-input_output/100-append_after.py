e3d99c34ce8e639fc424c7c2c8b47a166ba7450b
joseph maxime <mangajm004@gmail.com>
2022-08-04 11:26:53 +0000

    add

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Insert line to a file module """
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """function that inserts a line of text to a file,
+    after each line containing a specific string""" 
+
+    with open(filename, 'r', encoding="utf-8") as f:
+
+        lines = f.readlines()
+        text = ""
+
+        for line in lines:
+
+            text += line
+            if line.find(search_string) != -1:
+
+                text += new_string
+
+    with open(filename, 'w', encoding="utf-8") as f:
+
+        write_file = f.write(text)
