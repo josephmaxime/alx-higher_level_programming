731767b4bc02e7a14155ec533a6358ab7f1ac9eb
joseph maxime <mangajm004@gmail.com>
2022-08-09 19:26:27 +0000

    add rectangle file

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Module Rectangle models """
+from models.base import Base
 
 
 class Rectangle(Base):
@@ -8,7 +9,7 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initialize paramater"""
 
-        super().__init__(id) = id
+        super().__init__(id)
         self.__width = width
         self.__height = height
         self.x = x
