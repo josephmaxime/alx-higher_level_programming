874e3fc932af1909d6f11e0e9720ad7a3953d2f9
joseph maxime <mangajm004@gmail.com>
2022-08-09 18:01:46 +0000

    add base file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -14,5 +14,5 @@ class Base:
             self.id = id
 
         else:
-             __nb_objects += 1 
-            id = self.id
+            Base.__nb_objects += 1 
+            self.id = self.__nb_objects
